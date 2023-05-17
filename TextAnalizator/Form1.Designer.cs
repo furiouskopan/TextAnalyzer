581bab164c333debf1fb87fc3dd20b103dca8121
Gorgi <gorgikos20@gmail.com>
2023-05-17 18:30:52 +0200

    Initial commit

---
+++ TextAnalizator/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TextAnalizator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtFreq = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(41, 161);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(475, 407);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Californian FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(555, 275);
+            this.btnProcess.Margin = new System.Windows.Forms.Padding(4);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(226, 53);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Process Text";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtFreq
+            // 
+            this.txtFreq.Location = new System.Drawing.Point(820, 161);
+            this.txtFreq.Name = "txtFreq";
+            this.txtFreq.Size = new System.Drawing.Size(200, 405);
+            this.txtFreq.TabIndex = 3;
+            this.txtFreq.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter some text in the field below:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(856, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Word frequency";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Schoolbook", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(512, 40);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Text processing application";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 597);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFreq);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtInput);
+            this.Font = new System.Drawing.Font("Century Schoolbook", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Text Frequency Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.RichTextBox txtFreq;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TextAnalizator/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextAnalizator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            {
+                // Read the text from the input TextBox control and split it into words
+                string text = txtInput.Text;
+                string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Create a dictionary to store the words and their frequency
+                Dictionary<string, int> wordFreq = new Dictionary<string, int>();
+
+                // Update the dictionary with the words from the input text
+                foreach (string word in words)
+                {
+                    // Convert the word to lowercase and remove any non-letter characters
+                    string cleanedWord = new string(word.Where(char.IsLetter).ToArray()).ToLowerInvariant();
+
+                    // If the cleaned word is already in the dictionary, increase its frequency by 1
+                    if (wordFreq.ContainsKey(cleanedWord))
+                    {
+                        wordFreq[cleanedWord]++;
+                    }
+                    // Otherwise, add the cleaned word to the dictionary with a frequency of 1
+                    else
+                    {
+                        wordFreq.Add(cleanedWord, 1);
+                    }
+                }
+
+                // Determine the length of the longest word and the highest frequency
+                int maxWordLength = 0;
+                int maxFrequency = 0;
+                foreach (KeyValuePair<string, int> entry in wordFreq.OrderByDescending(pair => pair.Value))
+                {
+                    maxWordLength = Math.Max(maxWordLength, entry.Key.Length);
+                    maxFrequency = Math.Max(maxFrequency, entry.Value);
+                }
+
+                // Calculate the width of the frequency column based on the number of digits in the highest frequency
+                int frequencyWidth = maxFrequency.ToString().Length;
+
+                // Generate a string containing the words and their frequency, padded with spaces to align the columns
+                string results = "";
+                foreach (KeyValuePair<string, int> entry in wordFreq.OrderByDescending(pair => pair.Value))
+                {
+                    string word = entry.Key;
+                    string freq = entry.Value.ToString();
+
+                    // Pad the word and frequency with spaces to make them the same width as the longest word and the frequency column width, respectively
+                    word = word.PadRight(maxWordLength + 2);
+                    freq = freq.PadLeft(frequencyWidth + 1);
+
+                    // Add the word and frequency to the output with a newline character between them
+                    results += word + freq + Environment.NewLine;
+                }
+
+                // Display the results in the output TextBox control
+                txtFreq.Text = results;
+
+                // Set the font of the output TextBox control to a fixed-width font like Courier New
+                txtFreq.Font = new Font("Courier New", txtFreq.Font.Size);
+            }
+        }
+    }
+}
